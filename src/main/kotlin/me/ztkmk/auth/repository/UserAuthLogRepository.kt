bd8d75be9e91c3eae188de0fc1e3e06f0081ba07
Kebron <ztkmkoo@gmail.com>
2021-03-22 04:29:47 +0900

    # remove unused comment

--- src/main/kotlin/me/ztkmk/auth/repository/UserAuthLogRepository.kt
+++ src/main/kotlin/me/ztkmk/auth/repository/UserAuthLogRepository.kt
@@ -11,22 +11,5 @@ import java.util.*
  */
 @Repository
 interface UserAuthLogRepository: JpaRepository<UserAuthLog, Long> {
-//    companion object {
-//        const val BASE_COLUMNS = """
-//            seq,
-//            cellphone,
-//            uuid,
-//            number,
-//            created
-//        """
-//
-//        const val CONDITION_CELLPHONE = "cellphone = :cellphone"
-//        const val CONDITION_UUID = "uuid = :uuid"
-//        const val CONDITION_CREATED = "created >= :created"
-//    }
-//
-//    @Query(value = "SELECT $BASE_COLUMNS FROM user_auth_logs WHERE $CONDITION_CELLPHONE AND $CONDITION_CREATED")
-//    fun findOneByCellphoneIfRequested(@Param("cellphone") cellphone: String, @Param("created") created: Date): UserAuthLog?
-
     fun findOneByCellphoneAndCreatedGreaterThan(cellphone: String, created: Date): UserAuthLog?
 }
